694df7c10c4d0686a1c0def5e6f03cf063eab8bc
zubairkachi <kachizubairhussain0@gmail.com>
2025-07-30 18:42:24 +0530

    Add files via upload

---
+++ Mathematical Operations.py
@@ -0,0 +1,10 @@
+firstnumber=int(input("Enter your first name: "))
+secondnumber=int(input("Enter your second name: "))
+add=firstnumber+secondnumber
+sub=firstnumber-secondnumber
+mul=firstnumber*secondnumber
+div=firstnumber/secondnumber
+print("Your addition is",add)
+print("Your subtraction is",sub)
+print("Your multiplication is",mul)
+print("Your division is",div)
---
+++ Personalized Greeting.py
@@ -0,0 +1,4 @@
+firstname=str(input("Enter your first name: "))
+secondname=str(input("Enter your second name: "))
+greeting=firstname+secondname
+print("Hello,\t",greeting,"!","Welcome to the python program")
